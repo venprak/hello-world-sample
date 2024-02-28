76bd50efd59940b77e43b800651f339da22786e5
Venkat <venprak@yahoo.com>
2024-02-28 12:32:53 +0530

    Created new sample python program for Hello World

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:3.9.0
+ADD HelloWorld.py .
+CMD ["python", "./HelloWorld.py"]
+
---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+def sayhello(name, place):
+    print("!!!!!!!!!!!!Hello World!!!!!!!!!!!!!!! from "+name+" , "+place)
+     
+
+sayhello("Venkat","Bangalore,INDIA")
+
+
